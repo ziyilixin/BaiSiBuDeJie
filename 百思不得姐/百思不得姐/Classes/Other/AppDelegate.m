333383803e8cf2c056431156c2bb41199b8da19a
chaofengwang <1559878532@qq.com>
2017-08-01 23:19:23 +0800

    推送引导

--- 百思不得姐/百思不得姐/Classes/Other/AppDelegate.m
+++ 百思不得姐/百思不得姐/Classes/Other/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "BSTabBarController.h"
+#import "BSPushGuideView.h"
 
 @interface AppDelegate ()
 
@@ -27,6 +28,9 @@
 
     //显示窗口
     [self.window makeKeyAndVisible];
+
+    //显示推送引导
+    [BSPushGuideView show];
 
     return YES;
 }
